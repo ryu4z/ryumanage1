f3c0ff9a996093b128835314f2903806953717c4
ryu4z <105985703+ryu4z@users.noreply.github.com>
2022-06-14 16:18:07 +0700

    Update logomaker.py

--- EmikoRobot/modules/logomaker.py
+++ EmikoRobot/modules/logomaker.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Emiko Robot](https://t.me/EmiexRobot)",
+            caption="Logo by [Killua Robot](http://t.me/ryusupport_bot)",
             force_document=False,
         )
         os.remove(flnme)
